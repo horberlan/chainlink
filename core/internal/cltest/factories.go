eb7363a541e79da4e271e13a7a6c3ecbbd9a4669
connorwstein <connor.stein@mail.mcgill.ca>
2021-03-12 11:00:14 -0500

    nosec

--- core/internal/cltest/factories.go
+++ core/internal/cltest/factories.go
@@ -774,6 +774,7 @@ func MustInsertPipelineRun(t *testing.T, db *gorm.DB) pipeline.Run {
 }
 
 func MustInsertUnfinishedPipelineTaskRun(t *testing.T, store *strpkg.Store, pipelineRunID int64) pipeline.TaskRun {
+	/* #nosec G404 */
 	p := pipeline.TaskRun{DotID: strconv.Itoa(mathrand.Int()), PipelineRunID: pipelineRunID}
 	require.NoError(t, store.DB.Create(&p).Error)
 	return p
